807f1f113d61a5837955b5fdff789f83a9d60665
mathrick <mathrick@gmail.com>
2024-10-25 11:55:19 -0700

    More work on external_files

--- addons/external_files/plugin.gd
+++ addons/external_files/plugin.gd
@@ -4,73 +4,78 @@ const util = preload("util.gd")
 
 const LINK_FILE_NAME = ".external_files"
 
-class DirIter:
-	var path: String
-	var dir: DirAccess
-	var children: Array[String] = []
-	var _idx: int = 0
-
-	# Work around bugs in 4.2
-	func iter():
-		return self
-
-	func _init(_path):
-		path = _path
-
-	func _iter_init(arg):
-		dir = DirAccess.open(path)
-		if not dir:
-			printerr("Could not open directory %s: %s" % [path, DirAccess.get_open_error()])
-			return false
-		dir.include_hidden = true
-		_idx = 0
-		children.assign(dir.get_directories())
-		return children.size() > 0
-
-	func _iter_next(arg):
-		_idx += 1
-		return _idx < children.size()
-
-	func _iter_get(arg):
-		return children[_idx]
-
 func find_external_links(root_path: String) -> Array[String]:
 	var dirs: Array[String] = []
 	var link_file = root_path.path_join(LINK_FILE_NAME)
 
 	if FileAccess.file_exists(link_file):
 		dirs.append(link_file)
-	for dir in DirIter.new(root_path).iter():
+	for dir in util.DirIter.new(root_path).iter():
 		dirs.append_array(find_external_links(root_path.path_join(dir)))
 	return dirs
 
-func _test_hg_globs():
-	for rooted in [true, false]:
-		prints("rooted" if rooted else "not rooted:")
-		for test in [
-			["asd", ["asd", "asd.png", "foo", "foo/asd", "foo/bar", "foo/bar.txt"]],
-			["???", ["asd", "asd.png", "foo", "foo/asd", "foo/bar", "foo/bar.txt"]],
-			["???.txt", ["asd", "asd.png", "foo.txt", "foo/bar", "foo/bar.txt"]],
-			["*.png", ["asd", "asd.png", "foo.png", "*?.png", "bar.tar.png", "baz.txt", "foo/bar.png", "foo/bar/baz.png"]],
-			["foo/*/*.png", ["asd", "asd.png", "foo/bar.png", "foo/bar/baz/quux.png", "foo/bar/baz.png", "foo/bar/*?.png", "foo/bar/bar.tar.png", "foo/bar/baz.txt"]],
-			["foo/**/*.png", ["asd", "asd.png", "foo/bar.png", "foo/bar/baz/quux.png", "foo/bar/baz.png", "foo/bar/*?.png", "foo/bar/bar.tar.png", "foo/bar/baz.txt"]],
-			["foo/**.png", ["asd", "asd.png", "foo/bar.png", "foo/bar/baz/quux.png", "foo/bar/baz.png", "foo/bar/*?.png", "foo/bar/bar.tar.png", "foo/bar/baz.txt"]],
-		]:
-			var glob = test[0]
-			var cands = test[1]
-			var compiled = util.compile_hg_glob(glob, rooted)
-			for cand in cands:
-				prints("  ", "%-15s" % glob, "|", "%-25s" % cand, "|", compiled.search(cand) != null)
-			prints()
+func get_globs(pattern_file: String) -> Array:
+	var result: Array = []
+	var file = FileAccess.open(pattern_file, FileAccess.READ)
+	var dir: String = ""
+	var mode_regex = RegEx.new()
+	mode_regex.compile(r"^syntax: *(\w+)$")
+	var root_regex = RegEx.new()
+	root_regex.compile(r"## *root: *(.*)$")
+	var mode: String = "regexp"
+
+	var line: String
+	var lineno: int = 0
+	while not file.eof_reached():
+		line = file.get_line().strip_edges()
+		lineno += 1
+		if not line:
+			continue
+
+		var _match = mode_regex.search(line)
+		if _match:
+			mode = _match.get_string(1)
+			if mode not in ["glob", "rootglob", "regexp"]:
+				printerr("Invalid syntax choice '%s' in '%s':%d" % [mode, pattern_file, lineno])
+				return []
+			continue
+
+		_match = root_regex.search(line)
+		if _match:
+			if not _match.get_string(1):
+				printerr("No path given in root directive in '%s':%d" % [pattern_file, lineno])
+				return []
+			dir = "/".join(
+				ProjectSettings.globalize_path("res://").split("/").slice(0, -1)
+			).path_join(
+				_match.get_string(1)
+			)
+			continue
+
+		if line.begins_with("#"):
+			continue
+		if not dir:
+			printerr("Missing root directive in '%s':%d" % [pattern_file, lineno])
+			return []
+
+		var glob: RegEx
+		if mode in ["glob", "rootglob"]:
+			glob = util.compile_hg_glob(line, mode == "rootglob")
+		else:
+			glob = RegEx.new()
+			glob.compile("(^|/)%s" % line)
+		result.append([dir, glob])
+	return result
 
 func scan_and_update_project() -> void:
+	var globs: Array = []
 	for file in find_external_links("res://"):
-		prints("found file", file)
+		globs.append_array(get_globs(file))
 
-	_test_hg_globs()
+	prints(ProjectSettings.globalize_path("res://../../renpy"), globs)
 
 func _enter_tree():
-	add_tool_menu_item("Rescan external files", scan_and_update_project)
+	add_tool_menu_item("Re-import external files", scan_and_update_project)
 	scan_and_update_project()
 
 func _exit_tree():
--- addons/external_files/util.gd
+++ addons/external_files/util.gd
@@ -1,6 +1,36 @@
 @tool
 extends Node
 
+class DirIter:
+	var path: String
+	var dir: DirAccess
+	var children: Array[String] = []
+	var _idx: int = 0
+
+	# Work around bugs in 4.2
+	func iter():
+		return self
+
+	func _init(_path):
+		path = _path
+
+	func _iter_init(arg):
+		dir = DirAccess.open(path)
+		if not dir:
+			printerr("Could not open directory %s: %s" % [path, DirAccess.get_open_error()])
+			return false
+		dir.include_hidden = true
+		_idx = 0
+		children.assign(dir.get_directories())
+		return children.size() > 0
+
+	func _iter_next(arg):
+		_idx += 1
+		return _idx < children.size()
+
+	func _iter_get(arg):
+		return children[_idx]
+
 # (?aP) is PCRE2_EXTRA_ASCII_POSIX, which guarantees ASCII processing
 const _ALNUM = "(?aP)^[:alnum:]$"
 static var alnum_regex: RegEx
@@ -50,7 +80,7 @@ static func compile_hg_glob(glob: String, rooted: bool = true) -> RegEx:
 				return null
 			"}" when in_alternative:
 				alternatives.append(current_alternative)
-				patterns.append("({})".format(["|".join(alternatives)]))
+				patterns.append("({0})".format(["|".join(alternatives)]))
 				current_alternative = ""
 				alternatives = []
 			var c when in_alternative:
@@ -68,7 +98,30 @@ static func compile_hg_glob(glob: String, rooted: bool = true) -> RegEx:
 	add_pattern.call("")
 	var regex = RegEx.new()
 	regex.compile("{0}{1}$".format([
-		"^" if rooted else "",
+		"^" if rooted else "(^|/)",
 		"".join(patterns)
 	]))
 	return regex
+
+static func _test_hg_globs():
+	for rooted in [true, false]:
+		prints("rooted" if rooted else "not rooted:")
+		for test in [
+			["asd", ["asd", "asd.png", "foo", "foo/asd", "foo/bar", "foo/aaasd", "foo/bar.txt"]],
+			["???", ["asd", "asd.png", "foo", "foo/asd", "foo/bar", "foo/quux", "foo/bar.txt"]],
+			["???.txt", ["asd", "asd.png", "foo.txt", "foo/bar", "foo/bar.txt"]],
+			["*.png", ["asd", "asd.png", "foo.png", "*?.png", "bar.tar.png", "baz.txt", "foo/bar.png", "foo/bar/baz.png"]],
+			["*.{png,txt}", ["asd", "asd.png", "foo.png", "*?.png", "bar.tar.png", "baz.txt", "foo/bar.png", "foo/bar/baz.png"]],
+			["*.{tar,gz}", ["asd", "asd.png", "foo.png", "*?.png", "bar.tar.png", "baz.txt", "foo/bar.png", "foo/bar/baz.png"]],
+			["foo/*/*.png", ["asd", "asd.png", "foo/bar.png", "foo/bar/baz/quux.png", "foo/bar/baz.png", "foo/bar/*?.png", "foo/bar/bar.tar.png", "foo/bar/baz.txt"]],
+			["foo/**/*.png", ["asd", "asd.png", "foo/bar.png", "foo/bar/baz/quux.png", "foo/bar/baz.png", "foo/bar/*?.png", "foo/bar/bar.tar.png", "foo/bar/baz.txt"]],
+			["foo/**.png", ["asd", "asd.png", "foo/bar.png", "foo/bar/baz/quux.png", "foo/bar/baz.png", "foo/bar/*?.png", "foo/bar/bar.tar.png", "foo/bar/baz.txt"]],
+			["foo/**.{png,txt}", ["asd", "asd.png", "foo/bar.png", "foo/bar/baz/quux.png", "foo/bar/foo/quux.png", "foo/bar/baz.png", "foo/bar/*?.png", "foo/bar/bar.tar.png", "foo/bar/baz.txt"]],
+			["foo/**.{tar,gz}", ["asd", "asd.png", "foo/bar.png", "foo/bar/baz/quux.png", "foo/bar/foo/quux.png", "foo/bar/baz.png", "foo/bar/*?.png", "foo/bar/bar.tar.png", "foo/bar/baz.txt"]],
+		]:
+			var glob = test[0]
+			var cands = test[1]
+			var compiled = compile_hg_glob(glob, rooted)
+			for cand in cands:
+				prints("  ", "%-18s" % glob, "|", "%-25s" % cand, "|", "%-20s" % compiled.get_pattern(), "|", compiled.search(cand) != null)
+			prints()
